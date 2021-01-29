232ea7d994ec84cc93feab69e9267662f5084460
Matheus Moura <math.moura@hotmail.com>
2021-01-29 11:06:48 +0100

    trombinoscope with flags

--- js/main.js
+++ js/main.js
@@ -1,22 +1,43 @@
 console.log("Hello")
 
-var tab = ["fulano 1","fulano 2","fulano 3","fulano 4"];
-var numCol = 3;
-var gridTromb = document.getElementById("columnTromb").getElementsByTagName("div")[0];
-for(let i = 0; i < Math.ceil(tab.length/numCol)*numCol;i++){
-    
-    if(i%numCol==0){
-        let row = document.createElement("div");
-        gridTromb.appendChild(row).className = "row";
-    }
-    let col = document.createElement("div");
-    let line = gridTromb.getElementsByClassName("row")[Math.floor(i/numCol)]
-    line.appendChild(col).className = "col-sm";
-    if(i<tab.length){
-        line.getElementsByClassName("col-sm")[i%numCol].innerHTML = tab[i]
-    }
-    
+console.log(infoMembres)
 
-
-
+var gridTromb = document.getElementById("columnTromb").getElementsByClassName("col")[0];
+for (var el in infoMembres) {
+    let row = gridTromb.appendChild(document.createElement("div"))
+    row.className = "row row-section";
+    let col = row.appendChild(document.createElement("div"))
+    col.className = "col";
+    createGrid(infoMembres[el]["membres"], infoMembres[el]["numCol"], col)
 }
+
+
+function createGrid(tab, numCol, pos) {
+    for (let i = 0; i < Math.ceil(tab.length / numCol) * numCol; i++) {
+        if (i % numCol == 0) {
+            pos.appendChild(document.createElement("div")).className = "row";
+        }
+        let line = pos.getElementsByClassName("row")[Math.floor(i / numCol)]
+        line.appendChild(document.createElement("div")).className = "col-sm";
+        if (i < tab.length) {
+            imgFlag = ""
+            if(tab[i]["flag"]!=""){
+                imgFlag = '<img src="./img/drapeaux/'+tab[i]["flag"]+'.png" alt="Flag" class="flag">';
+            }
+            
+            line.getElementsByClassName("col-sm")[i % numCol].innerHTML = 
+            '<div class="avatar-img">'+
+            '<img src="./img/photos/photo1.jpg" alt="Avatar" class="avatar">'+
+            imgFlag+
+            '</div>'+
+            '<div class="avatar-text">'+
+            '<p class="nom-text">'+tab[i]["nom"]+'</p>'+
+            '<p class="nom-pos">'+tab[i]["occupation"]+'</p>'+
+            '</div>'
+        }
+    }
+}
+
+
+
+
---
+++ json/infoMembres.js
@@ -0,0 +1,85 @@
+infoMembres = {
+        "bureau": {
+            "numCol": 4,
+            "membres": [
+                {
+                    "nom": "RECKER Tobias",
+                    "occupation": "Prez",
+                    "photo": "",
+                    "flag":"belgium"
+                },
+                {
+                    "nom": "JIMÉNEZ Victor",
+                    "occupation": "Vice-Prez",
+                    "photo": "",
+                    "flag":"mexico"
+                },
+                {
+                    "nom": "ALMEIDA Juliana",
+                    "occupation": "Screz",
+                    "photo": "",
+                    "flag":"brazil"
+                },
+                {
+                    "nom": "LIU Victor",
+                    "occupation": "Trez",
+                    "photo": "",
+                    "flag":"china"
+                }
+            ]
+        },
+        "respo": {
+            "numCol": 5,
+            "membres": [
+                {
+                    "nom": "s",
+                    "occupation": "Prez",
+                    "photo": "",
+                    "flag":""
+                },
+                {
+                    "nom": "d",
+                    "occupation": "Vice Prez",
+                    "photo": "",
+                    "flag":""
+                },
+              
+            ]
+        },
+        "poleComm": {
+            "numCol": 5,
+            "membres": [
+                {
+                    "nom": "s",
+                    "occupation": "Prez",
+                    "photo": "",
+                    "flag":""
+                },
+                {
+                    "nom": "d",
+                    "occupation": "Vice Prez",
+                    "photo": "",
+                    "flag":""
+                },
+              
+            ]
+        },
+        "poleEvent": {
+            "numCol": 5,
+            "membres": [
+                {
+                    "nom": "alo",
+                    "occupation": "event1",
+                    "photo": "",
+                    "flag":""
+                },
+                {
+                    "nom": "deer",
+                    "occupation": "event2",
+                    "photo": "",
+                    "flag":""
+                },
+              
+            ]
+        }
+    }
