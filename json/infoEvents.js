1eec8c3003f0b3e15e472c7dba18f794ec26986c
Matheus Moura <math.moura@hotmail.com>
2021-02-04 13:13:55 +0100

    video, events and partenaire

---
+++ json/infoEvents.js
@@ -0,0 +1,17 @@
+infoEvents = [{
+    "title": "ola 1",
+    "date": "1 Janvier 2021",
+    "description": "skdss ds ds"
+}, {
+    "title": "ola 2",
+    "date": "1 Juin 2021",
+    "description": "skdss ds dds sd ds ds ds ds ds ds ds ds ds df  fd fd fd fd df df fd fd  df df df df s asa s"
+}, {
+    "title": "ola 3",
+    "date": "19 Julliet 2021",
+    "description": "skdss ds dsdsds dfddflk reere re re re ds ds d fd fd gf df d "
+}, {
+    "title": "ola 1",
+    "date": "27 Novembre 2021",
+    "description": "skdss ds ds"
+}]
